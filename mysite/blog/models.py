5b892ef70dcf533970513526293cba9b2fba3b70
Gabriel Araujo <gabrielsant95@gmail.com>
2023-07-31 11:06:23 -0300

    Corrected typo on models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250) # short label
-    body = models.models.TextField()
+    body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True) # DOB of post / Data de criação do post
     updated = models.DateTimeField(auto_now=True)
